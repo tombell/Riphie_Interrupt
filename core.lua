0631308656edf03cd19ea1c92d101f14b7860e63
Tom Bell <tomb@tomb.io>
2018-08-20 00:10:41 +0100

    Fix for BfA

--- core.lua
+++ core.lua
@@ -4,9 +4,10 @@ local cfg = L.cfg
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 frame:SetScript("OnEvent", function(self, event, ...)
-  if select(2, ...) ~= "SPELL_INTERRUPT" then return end
-  if select(5, ...) ~= UnitName("player") then return end
+  local _, event, _, _, sourceName, _, _, _, _, _, _, _, _, _, extraArg4, _, _, _, _, _, _ = CombatLogGetCurrentEventInfo()
 
-  local spellId = select(15, ...)
-  SendChatMessage("Interrupted " .. GetSpellLink(spellId), cfg.channel)
+  if event ~= "SPELL_INTERRUPT" then return end
+  if sourceName ~= UnitName("player") then return end
+
+  SendChatMessage("Interrupted " .. GetSpellLink(extraArg4), cfg.channel)
 end)
